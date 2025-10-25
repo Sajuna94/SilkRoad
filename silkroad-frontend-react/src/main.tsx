3a10bcb81e389df36d46f511478df3bb36621720
Sajuna94 <sajuna94@gmail.com>
2025-10-26 02:33:23 +0800

    test 404 html 5

--- silkroad-frontend-react/src/main.tsx
+++ silkroad-frontend-react/src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import '@/index.css'
 import App from '@/App'
 
+const redirect = sessionStorage.redirect;
+if (redirect && redirect !== window.location.pathname) {
+	sessionStorage.removeItem("redirect");
+	window.history.replaceState(null, "", redirect);
+}
+
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<App />
