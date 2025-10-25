d116d4a9734e74864b0877a644611fba6279dc38
Sajuna94 <sajuna94@gmail.com>
2025-10-26 01:35:25 +0800

    edit footer url

--- silkroad-frontend-react/src/layout/Footer.tsx
+++ silkroad-frontend-react/src/layout/Footer.tsx
@@ -12,9 +12,13 @@ export default function Footer() {
 				<div className={styles.right}>
 					<div className={styles.brand}>SilkRoad</div>
 					<nav className={styles.links}>
-						<Link to="/terms">Terms</Link>
-						<Link to="/privacy">Privacy</Link>
-						<a href="https://github.com" target="_blank" rel="noopener noreferrer">
+						<a href="https://www.canva.com/design/DAG2srcWvXg/sBX9HgzAxDytv1BVgdnE_A/edit" target="_blank" rel="noopener noreferrer">
+							Canva
+						</a>
+						<a href="https://onedrive.live.com/personal/8a32e11b977cc753/_layouts/15/Doc.aspx?sourcedoc=%7B4f676aaa-a9d4-495d-84c6-f2819ef5ef35%7D&action=default&redeem=aHR0cHM6Ly8xZHJ2Lm1zL3cvYy84YTMyZTExYjk3N2NjNzUzL0VhcHFaMF9VcVYxSmhNYnlnWjcxN3pVQlh2ZlhVRGlrMExEdHJaaVlrU3k1cmc_ZT1sc0dTdGc&slrid=80e9d2a1-608b-0000-098f-f6b193ee6ac8&originalPath=aHR0cHM6Ly8xZHJ2Lm1zL3cvYy84YTMyZTExYjk3N2NjNzUzL0VhcHFaMF9VcVYxSmhNYnlnWjcxN3pVQlh2ZlhVRGlrMExEdHJaaVlrU3k1cmc_cnRpbWU9S3dEdkV1d1Qza2c&CID=f4694c94-c31c-4b88-8533-e69d448a5778&_SRM=0:G:38" target="_blank" rel="noopener noreferrer">
+							Specification
+						</a>
+						<a href="https://github.com/Sajuna94/SilkRoad" target="_blank" rel="noopener noreferrer">
 							GitHub
 						</a>
 					</nav>
