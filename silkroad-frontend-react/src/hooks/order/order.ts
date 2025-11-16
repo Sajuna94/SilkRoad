de8929f9056f0f442614495fb72ee254c7d7fae8
ChengAnHsi <t112590007@ntut.org.tw>
2025-11-17 00:37:39 +0800

    order page update

--- silkroad-frontend-react/src/hooks/order/order.ts
+++ silkroad-frontend-react/src/hooks/order/order.ts
@@ -1,23 +1,18 @@
-import { addOrder } from "@/api/order";
+import { addOrder, getOrders } from "@/api/order";
 import type { InsertOrderInput, Order } from "@/types/order";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import type { AxiosError } from "axios";
 
-export const useOrder = () => {
-    return useQuery({
-        queryKey: ["order"],
-        queryFn: async () => {
-            return { "messsage": "fjoifefjoief" };
-        },
-    });
-}
+export const useOrders = (customerId: number) => {
+  return useQuery<Order[], AxiosError>({
+    queryKey: ["orders", customerId],
+    queryFn: () => getOrders(customerId),
+    enabled: !!customerId,
+  });
+};
 
 export const useInsertOrder = () => {
-    // const queryClient = useQueryClient();
-
-    return useMutation<Order, AxiosError, InsertOrderInput>({
-        mutationFn: addOrder,
-        onSuccess: () => {
-        },
-    });
+  return useMutation<Order, AxiosError, InsertOrderInput>({
+    mutationFn: addOrder,
+  });
 };
--- silkroad-frontend-react/src/layout/Header.tsx
+++ silkroad-frontend-react/src/layout/Header.tsx
@@ -2,17 +2,18 @@ import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 
 export default function Header() {
-    return (
-        <header className={styles.navbar}>
-            <div className={styles.navbarLogo}>
-                <Link to="/">SilkRoad</Link>
-            </div>
-            <nav className={styles.navbarLinks}>
-                <Link to="/">Home</Link>
-                <Link to="/cart">Cart</Link>
-                <Link to="/about">About</Link>
-                <Link to="/login">Login</Link>
-            </nav>
-        </header>
-    );
+  return (
+    <header className={styles.navbar}>
+      <div className={styles.navbarLogo}>
+        <Link to="/">SilkRoad</Link>
+      </div>
+      <nav className={styles.navbarLinks}>
+        <Link to="/">Home</Link>
+        <Link to="/cart">Cart</Link>
+        <Link to="/orders">Order</Link>
+        <Link to="/about">About</Link>
+        <Link to="/login">Login</Link>
+      </nav>
+    </header>
+  );
 }
