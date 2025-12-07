4ad4707340b6b04ad5dbc59fe871126151446959
ChengAnHsi <t112590007@ntut.org.tw>
2025-12-07 23:16:25 +0800

    add vendor dashboard title

--- silkroad-frontend-react/src/pages/Vendor/Dashboard/index.tsx
+++ silkroad-frontend-react/src/pages/Vendor/Dashboard/index.tsx
@@ -5,51 +5,53 @@ import OverviewTab from "./tabs/Overview";
 import ProductTab from "./tabs/Product";
 
 export default function VendorDashboardPage() {
-	const navigate = useNavigate();
-	const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-	const tabs = [
-		{ id: "1", label: "資訊總覽", element: <OverviewTab /> },
-		{ id: "2", label: "商品管理", element: <ProductTab /> },
-		{ id: "3", label: "訂單管理", element: <OrderTab /> },
-	];
+  const tabs = [
+    { id: "1", label: "資訊總覽", element: <OverviewTab /> },
+    { id: "2", label: "商品管理", element: <ProductTab /> },
+    { id: "3", label: "訂單管理", element: <OrderTab /> },
+  ];
 
-	const currentId = location.hash.replace("#", "") || tabs[0].id;
+  const currentId = location.hash.replace("#", "") || tabs[0].id;
 
-	return (
-		<div className={styles.dashboard}>
-			<aside className={styles.sidebar}>
-				<ul>
-					{tabs.map((t) => (
-						<li
-							key={t.id}
-							onClick={() => navigate(`#${t.id}`)}
-							className={`${styles.tabButton} ${currentId === t.id ? "active" : ""
-								}`}
-						>
-							{t.label}
-						</li>
-					))}
-				</ul>
-			</aside>
+  return (
+    <div className={styles.dashboard}>
+      <aside className={styles.sidebar}>
+        <div className={styles.headerTitle}>Vendor Dashboard</div>
+        <ul>
+          {tabs.map((t) => (
+            <li
+              key={t.id}
+              onClick={() => navigate(`#${t.id}`)}
+              className={`${styles.tabButton} ${
+                currentId === t.id ? "active" : ""
+              }`}
+            >
+              {t.label}
+            </li>
+          ))}
+        </ul>
+      </aside>
 
-			<main className={styles.main}>
-				{tabs.find((t) => t.id === currentId)?.element ?? null}
-			</main>
-		</div>
+      <main className={styles.main}>
+        {tabs.find((t) => t.id === currentId)?.element ?? null}
+      </main>
+    </div>
 
-		// <div className={styles["container"]}>
-		//   <aside>
-		//     <ul>
-		//       {tabs.map((t) => (
-		//         <li key={t.id} onClick={() => navigate(`#${t.id}`)}>
-		//           {t.label}
-		//         </li>
-		//       ))}
-		//     </ul>
-		//   </aside>
+    // <div className={styles["container"]}>
+    //   <aside>
+    //     <ul>
+    //       {tabs.map((t) => (
+    //         <li key={t.id} onClick={() => navigate(`#${t.id}`)}>
+    //           {t.label}
+    //         </li>
+    //       ))}
+    //     </ul>
+    //   </aside>
 
-		//   <main>{tabs.find((t) => t.id === currentId)?.element ?? null}</main>
-		// </div>
-	);
+    //   <main>{tabs.find((t) => t.id === currentId)?.element ?? null}</main>
+    // </div>
+  );
 }
