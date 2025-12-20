561541023a75d3cc7e86b86f75c9c23ca41c5a7e
Sajuna94 <sajuna94@gmail.com>
2025-12-20 16:13:12 +0800

    refactor: update user session handling and improve profile component structure

--- silkroad-frontend-react/src/hooks/auth/user.ts
+++ silkroad-frontend-react/src/hooks/auth/user.ts
@@ -5,25 +5,24 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 type LoginReq = { email: string; password: string };
 
 type RegisterReq = {
-  email: string;
-  password: string;
-  phone_number: string;
-  role: string;
+	email: string;
+	password: string;
+	phone_number: string;
+	role: string;
 };
 type RegisterVendorReq = {
-  manager: { name: string; email: string; phone_number: string };
-  vendor: { name: string; address: string };
+	manager: { name: string; email: string; phone_number: string };
+	vendor: { name: string; address: string };
 };
 type RegisterCustomerReq = {
-  name: string;
-  address: string;
+	name: string;
+	address: string;
 };
 type RegisterRoleReq = RegisterVendorReq | RegisterCustomerReq;
 
 export const useLogin = () => {
-  const qc = useQueryClient();
+	const qc = useQueryClient();
 
-<<<<<<< HEAD
 	return useMutation<User, ApiErrorBody, LoginReq>({
 		mutationFn: async (payload) => {
 			const res = await api.post("/user/login", payload);
@@ -35,33 +34,20 @@ export const useLogin = () => {
 			qc.invalidateQueries({ queryKey: ['user'] });
 		},
 	});
-=======
-  return useMutation<User, ApiErrorBody, LoginReq>({
-    mutationFn: async (payload) => {
-      const res = await api.post("/user/login", payload);
-      return res.data.data[0];
-    },
-    onSuccess: (res) => {
-      console.log(`[${res.role}] Login successful:`, res);
-      qc.setQueryData(["current_user"], res);
-    },
-  });
->>>>>>> fe22923c930ec5818186d190b396f82a0290e0e3
 };
 
 export const useRegister = () => {
-  return useMutation<any, ApiErrorBody, RegisterReq>({
-    mutationFn: async (payload) => {
-      const res = await api.post("/user/register/guest", payload);
-      return res.data;
-    },
-  });
+	return useMutation<any, ApiErrorBody, RegisterReq>({
+		mutationFn: async (payload) => {
+			const res = await api.post("/user/register/guest", payload);
+			return res.data;
+		},
+	});
 };
 
 export const useRegisterRole = (role: UserRole) => {
-  const qc = useQueryClient();
+	const qc = useQueryClient();
 
-<<<<<<< HEAD
 	return useMutation<User, ApiErrorBody, RegisterRoleReq>({
 		mutationFn: async (payload) => {
 			const res = await api.post(`/user/register/${role}`, payload);
@@ -75,38 +61,23 @@ export const useRegisterRole = (role: UserRole) => {
 			console.error("註冊失敗:", error.response?.data);
 		}
 	});
-=======
-  return useMutation<User, ApiErrorBody, RegisterRoleReq>({
-    mutationFn: async (payload) => {
-      const res = await api.post(`/user/register/${role}`, payload);
-      return res.data.data[0];
-    },
-    onSuccess: (res) => {
-      qc.setQueryData(["current_user"], res);
-    },
-    onError: (error) => {
-      console.error("註冊失敗:", error.response?.data);
-    },
-  });
->>>>>>> fe22923c930ec5818186d190b396f82a0290e0e3
 };
 
 export const useUser = () => {
-  return useQuery<User, ApiErrorBody>({
-    queryKey: ["user"],
-    queryFn: async () => {
-      const res = await api.get("/user/me");
-      return res.data.data;
-    },
-    retry: false,
-    refetchOnWindowFocus: false,
-  });
+	return useQuery<User, ApiErrorBody>({
+		queryKey: ["user"],
+		queryFn: async () => {
+			const res = await api.get("/user/me");
+			return res.data.data;
+		},
+		retry: false,
+		refetchOnWindowFocus: false,
+	});
 };
 
 export const useLogout = () => {
-  const qc = useQueryClient();
+	const qc = useQueryClient();
 
-<<<<<<< HEAD
 	return useMutation<void, ApiErrorBody>({
 		mutationFn: async () => {
 			await api.post("/user/logout");
@@ -123,32 +94,10 @@ export const useCurrentUser = () => {
 		queryKey: ["user"],
 		queryFn: async () => {
 			const res = await api.get("/user/current_user");
+			console.log(res);
 			return res.data.data;
 		},
 		retry: false,
 		refetchOnWindowFocus: false,
 	});
 }
-=======
-  return useMutation<void, ApiErrorBody>({
-    mutationFn: async () => {
-      await api.post("/user/logout");
-    },
-    onSuccess: () => {
-      qc.clear();
-    },
-  });
-};
-
-export const useCurrentUser = () => {
-  return useQuery<User, ApiErrorBody>({
-    queryKey: ["current_user"],
-    queryFn: async () => {
-      const res = await api.get("/user/current_user");
-      return res.data.data;
-    },
-    retry: false,
-    refetchOnWindowFocus: false,
-  });
-};
->>>>>>> fe22923c930ec5818186d190b396f82a0290e0e3
--- silkroad-frontend-react/src/layout/Header.tsx
+++ silkroad-frontend-react/src/layout/Header.tsx
@@ -1,16 +1,60 @@
 import { useState, useRef, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styles from "./Header.module.scss";
-<<<<<<< HEAD
-import { useCurrentUser, useLogout } from "@/hooks/auth/user";
+// import { type User } from "@/types/user";
+// import { useQueryClient } from "@tanstack/react-query";
+import { useLogout, useCurrentUser } from "@/hooks/auth/user";
+
+const DEFAULT_AVATAR =
+    "https://ui-avatars.com/api/?background=random&color=fff&name=";
 
 export default function Header() {
-    const logout = useLogout();
+    // const user: User = {
+    //     id: 2,
+    //     name: "test",
+    //     email: "test@gmail.com",
+    //     phone_number: "2222",
+    //     role: "admin",
+    //     created_at: "22020/22/22"
+    // };
 
+    //   const qc = useQueryClient();
+    //   const user = qc.getQueryData<User>(["user"]);
+    const logout = useLogout();
+    const navigate = useNavigate();
     const { data: user } = useCurrentUser();
+    // if (!user) {
+    // logout
+    // }
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const menuRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+                setIsMenuOpen(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+    const handleLinkClick = () => {
+        setIsMenuOpen(false);
+    };
+
+    const handleLogout = () => {
+        logout.mutate();
+        setIsMenuOpen(false);
+        navigate("/home");
+    };
 
     return (
-        <header className={styles['header-warp']}>
+        <header className={styles["header-warp"]}>
             <h1>
                 <Link to="/home">SilkRoad</Link>
             </h1>
@@ -18,156 +62,70 @@ export default function Header() {
                 <ul>
                     <li><Link to="/about">關於我們</Link></li>
                     <li><Link to="/vendor">Vendor</Link></li>
-                </ul>
-                <ul>
-                    {/* {user.role === "admin" && (
-                        <li><Link to="/admin">Admin</Link></li>
-                    )} */}
-                    <li><Link to="/user/orders">訂單紀錄</Link></li>
                     <li><Link to="/cart">查看購物車</Link></li>
-
-                    {user ? (
-                        <li onClick={() => { logout.mutate(); }}><a>登出</a></li>
+                </ul>
+                <ul className={styles.authSection}>
+                    {!user ? (
+                        // 未登入
+                        <li>
+                            <Link to="/login">登入</Link>
+                        </li>
                     ) : (
-                        <li><Link to="/login">登入</Link></li>
+                        // 已登入：顯示頭像與下拉選單
+                        // 綁定 ref 以偵測點擊範圍
+                        <div className={styles.userMenuContainer} ref={menuRef}>
+                            <div
+                                className={styles.avatarCircle}
+                                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                            >
+                                <img src={`${DEFAULT_AVATAR}${user.name}`} alt={user.name} />
+                            </div>
+
+                            {isMenuOpen && (
+                                <div className={styles.dropdownMenu}>
+                                    <div className={styles.userInfo}>
+                                        <div className={styles.userName}>{user.name}</div>
+                                        <div className={styles.userEmail}>{user.email}</div>
+                                    </div>
+
+                                    {user.role === "admin" && (
+                                        <Link
+                                            to="/admin"
+                                            className={styles.menuItem}
+                                            onClick={handleLinkClick}
+                                        >
+                                            管理員後台
+                                        </Link>
+                                    )}
+
+                                    <Link
+                                        to="/user/orders"
+                                        className={styles.menuItem}
+                                        onClick={handleLinkClick}
+                                    >
+                                        我的訂單
+                                    </Link>
+
+                                    <Link
+                                        to="/user/profile"
+                                        className={styles.menuItem}
+                                        onClick={handleLinkClick}
+                                    >
+                                        個人資料設定
+                                    </Link>
+
+                                    <div
+                                        className={`${styles.menuItem} ${styles.logoutBtn}`}
+                                        onClick={handleLogout}
+                                    >
+                                        登出
+                                    </div>
+                                </div>
+                            )}
+                        </div>
                     )}
                 </ul>
             </nav>
         </header>
     );
-=======
-// import { type User } from "@/types/user";
-// import { useQueryClient } from "@tanstack/react-query";
-import { useLogout, useCurrentUser } from "@/hooks/auth/user";
-
-const DEFAULT_AVATAR =
-  "https://ui-avatars.com/api/?background=random&color=fff&name=";
-
-export default function Header() {
-  // const user: User = {
-  //     id: 2,
-  //     name: "test",
-  //     email: "test@gmail.com",
-  //     phone_number: "2222",
-  //     role: "admin",
-  //     created_at: "22020/22/22"
-  // };
-
-  //   const qc = useQueryClient();
-  //   const user = qc.getQueryData<User>(["user"]);
-  const logout = useLogout();
-  const navigate = useNavigate();
-  const { data: user } = useCurrentUser();
-  // if (!user) {
-  // logout
-  // }
-
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const menuRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
-        setIsMenuOpen(false);
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
-
-  const handleLinkClick = () => {
-    setIsMenuOpen(false);
-  };
-
-  const handleLogout = () => {
-    logout.mutate();
-    setIsMenuOpen(false);
-    navigate("/home");
-  };
-
-  return (
-    <header className={styles["header-warp"]}>
-      <h1>
-        <Link to="/home">SilkRoad</Link>
-      </h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/about">關於我們</Link>
-          </li>
-          <li>
-            <Link to="/vendor">Vendor</Link>
-          </li>
-          <li>
-            <Link to="/cart">查看購物車</Link>
-          </li>
-        </ul>
-        <ul className={styles.authSection}>
-          {!user ? (
-            // 未登入
-            <li>
-              <Link to="/login">登入</Link>
-            </li>
-          ) : (
-            // 已登入：顯示頭像與下拉選單
-            // 綁定 ref 以偵測點擊範圍
-            <div className={styles.userMenuContainer} ref={menuRef}>
-              <div
-                className={styles.avatarCircle}
-                onClick={() => setIsMenuOpen(!isMenuOpen)}
-              >
-                <img src={`${DEFAULT_AVATAR}${user.name}`} alt={user.name} />
-              </div>
-
-              {isMenuOpen && (
-                <div className={styles.dropdownMenu}>
-                  <div className={styles.userInfo}>
-                    <div className={styles.userName}>{user.name}</div>
-                    <div className={styles.userEmail}>{user.email}</div>
-                  </div>
-
-                  {user.role === "admin" && (
-                    <Link
-                      to="/admin"
-                      className={styles.menuItem}
-                      onClick={handleLinkClick}
-                    >
-                      管理員後台
-                    </Link>
-                  )}
-
-                  <Link
-                    to="/user/orders"
-                    className={styles.menuItem}
-                    onClick={handleLinkClick}
-                  >
-                    我的訂單
-                  </Link>
-
-                  <Link
-                    to="/user/profile"
-                    className={styles.menuItem}
-                    onClick={handleLinkClick}
-                  >
-                    個人資料設定
-                  </Link>
-
-                  <div
-                    className={`${styles.menuItem} ${styles.logoutBtn}`}
-                    onClick={handleLogout}
-                  >
-                    登出
-                  </div>
-                </div>
-              )}
-            </div>
-          )}
-        </ul>
-      </nav>
-    </header>
-  );
->>>>>>> fe22923c930ec5818186d190b396f82a0290e0e3
 }
