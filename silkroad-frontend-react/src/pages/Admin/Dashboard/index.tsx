fa98855b9f3d647934eb3d8d7f465a0f0cddc91a
ChengAnHsi <t112590007@ntut.org.tw>
2025-12-27 17:21:07 +0800

    update admin dashboard page

--- silkroad-frontend-react/src/pages/Admin/Dashboard/index.tsx
+++ silkroad-frontend-react/src/pages/Admin/Dashboard/index.tsx
@@ -1,21 +1,18 @@
 import { useState } from "react";
 import UserManagement from "./Tabs/UserManagement";
 import SystemDashboard from "./Tabs/SystemDashboard";
-import VendorManagement from "./Tabs/VendorManagement";
 import styles from "./Dashboard.module.scss";
 
 export default function Dashboard() {
   const [activeTab, setActiveTab] = useState("user");
 
   const tabs = [
-    { id: "user", name: "Customer Management" },
+    { id: "user", name: "User Management" },
     { id: "system", name: "System Announcements" },
-    { id: "vendor", name: "Vendor Management" },
   ];
 
   return (
     <div className={`${styles.dashboard} `}>
-      {/* Sidebar */}
       <aside className={`${styles.sidebar}`}>
         <div className={styles.headerTitle}>Admin Dashboard</div>
         <nav>
@@ -33,11 +30,9 @@ export default function Dashboard() {
         </nav>
       </aside>
 
-      {/* Main content */}
       <main className={`${styles.main}`}>
         {activeTab === "user" && <UserManagement />}
         {activeTab === "system" && <SystemDashboard />}
-        {activeTab === "vendor" && <VendorManagement />}
       </main>
     </div>
   );
